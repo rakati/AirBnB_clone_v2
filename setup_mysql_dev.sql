57f6963da12a946de31cd92c5800999350c58bb3
amine-zzr <aminezoukri@gmail.com>
2024-04-18 02:06:13 +0000

    a script that prepares a MySQL server for the project

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,12 @@
+-- a script that prepares a MySQL server for the project
+-- Create database if not exists
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
+-- Create user if not exists
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
+-- Grant privileges on hbnb_dev_db
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+
+-- Grant SELECT privilege on performance_schema
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
