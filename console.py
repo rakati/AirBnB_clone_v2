a19fd56c8d9934678bdead233abc68c4a4637aff
amine-zzr <aminezoukri@gmail.com>
2024-04-20 07:05:49 +0000

    modifing the do_all method to handle database storage

--- console.py
+++ console.py
@@ -217,6 +217,7 @@ class HBNBCommand(cmd.Cmd):
         print("Destroys an individual instance of a class")
         print("[Usage]: destroy <className> <objectId>\n")
 
+
     def do_all(self, args):
         """ Shows all objects, or all objects of a class"""
         print_list = []
@@ -226,14 +227,16 @@ class HBNBCommand(cmd.Cmd):
             if args not in HBNBCommand.classes:
                 print("** class doesn't exist **")
                 return
-            for k, v in storage._FileStorage__objects.items():
-                if k.split('.')[0] == args:
-                    print_list.append(str(v))
+            objects_dict = storage.all(args)
+            for obj_key, obj in objects_dict.items():
+                print_list.append(str(obj))
         else:
-            for k, v in storage._FileStorage__objects.items():
-                print_list.append(str(v))
+            objects_dict = storage.all()
+            for obj_key, obj in objects_dict.items():
+                print_list.append(str(obj))
 
         print(print_list)
+
 
     def help_all(self):
         """ Help information for the all command """
