6cf14372ac7554265378dbdf1126507354fc8eda
amine-zzr <aminezoukri@gmail.com>
2024-04-21 02:00:23 +0000

    fixing code

--- models/user.py
+++ models/user.py
@@ -2,20 +2,13 @@
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
-import models
 
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    if models.storage_type == 'db':
-        __tablename__ = 'users'
+    __tablename__ = 'users'
 
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
-    else:
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
