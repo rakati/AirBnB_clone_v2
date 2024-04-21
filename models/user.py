98714e6b5eb0a2a6ca9f8198a0d20709eb17bddc
amine-zzr <aminezoukri@gmail.com>
2024-04-21 01:42:59 +0000

    fixing code

--- models/user.py
+++ models/user.py
@@ -2,17 +2,20 @@
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
+import models
 
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    __tablename__ = 'users'
+    if models.storage_type == 'db':
+        __tablename__ = 'users'
 
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
-
-    def __init__(self, *args, **kwargs):
-        """Instantiates a new User"""
-        super().__init__(*args, **kwargs)
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
