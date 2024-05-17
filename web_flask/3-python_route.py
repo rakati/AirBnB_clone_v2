9f82344c46aa1d73284d9208f64d5406463670e3
rakati <ouhaddounouredine@gmail.com>
2024-05-17 22:16:30 +0100

    add route that accpet string parameter with default value

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+'''A simple module with flask app'''
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_route():
+    '''router for route url'''
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb_route():
+    '''router for route url'''
+    return 'HBNB'
+
+
+@app.route('/c/<string:text>', strict_slashes=False)
+def c_is_fun_route(text):
+    '''router for route url'''
+    import re
+    return 'C ' + re.sub('_', ' ', text)
+
+
+@app.route('/python/<string:text>', strict_slashes=False,
+           default={'text': 'is cool'})
+def python_is_cool_route(text):
+    '''router for route url'''
+    import re
+    return 'Python ' + re.sub('_', ' ', text)
+
+
+if __name__ == '__main__':
+    app.run(port=5000)
