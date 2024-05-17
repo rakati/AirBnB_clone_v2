1e47c209d0b3033795d27e499d98fd5a01125edf
rakati <ouhaddounouredine@gmail.com>
2024-05-17 22:51:03 +0100

    add a new route to display number and use template

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+'''A simple module with flask app'''
+from flask import Flask
+from flask import render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_route():
+    '''router for route url'''
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb_route():
+    '''router for route url'''
+    return 'HBNB'
+
+
+@app.route('/c/<string:text>', strict_slashes=False)
+def c_is_fun_route(text):
+    '''router for route url'''
+    import re
+    return 'C ' + re.sub('_', ' ', text)
+
+
+@app.route('/python', strict_slashes=False,
+           defaults={'text': 'is cool'})
+@app.route('/python/<string:text>', strict_slashes=False)
+def python_is_cool_route(text):
+    '''router for route url'''
+    import re
+    return 'Python ' + re.sub('_', ' ', text)
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def is_number_route(n):
+    '''router for route url'''
+    return str(n) + ' is a number'
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template_route(n):
+    '''router for route url'''
+    data = {
+        'n': n
+    }
+    return render_template('5-number.html', data=data)
+
+
+if __name__ == '__main__':
+    app.run(port=5000)
